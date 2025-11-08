10781983a2832833208e2502cf4a2e6cb7e4359f
SkSaikatAhmed <ahmedsheikhsaikat@gmail.com>
2025-11-08 11:43:00 +0530

    basic counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Header from './MyComponents/Header'
-import Footer from './MyComponents/Footer';
+// import Footer from './MyComponents/Footer';
 
 function App() {
   return (
     <>
       <Header/>
-      
-      <Footer/>
     </>
   );
 }
--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react';
+// import PropTypes from 'prop-types';
 
-export default function Header() {
+function Header(props) {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count+1);
+  const reset = () => setCount(0);
   return (
     <div>
-      Its a simple Header
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={reset}>Reset</button>
     </div>
   )
 }
+
+export default Header;
