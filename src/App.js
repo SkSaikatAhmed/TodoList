957eb09129b07a8953bd1101237b489cc31221ac
SkSaikatAhmed <ahmedsheikhsaikat@gmail.com>
2025-11-09 11:46:23 +0530

    Basic Calculator Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import './App.css'
 import Header from './MyComponents/Header'
 // import Footer from './MyComponents/Footer';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <Header/>
-    </>
+    </div>
   );
 }
 
--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,16 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import './Header.css'
 // import PropTypes from 'prop-types';
 
 function Header(props) {
   const [count, setCount] = useState(0);
+  useEffect(()=> {
+    document.title = `You Clickes ${count} times`;
+  }, [count]);
 
   const increment = () => setCount(count+1);
   const reset = () => setCount(0);
+  const decrement = () => setCount(count-1);
   return (
-    <div>
+    <div className='counter-card'>
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={reset} style={{marginLeft: '10px'}}>Reset</button>
+      <button onClick={decrement} disabled={count===0} style={{marginLeft: '10px'}}>Decrement</button>
     </div>
   )
 }
